e4ad393b49e0d79e9fbcf7cf5d450da4e4eb41cf
HideinMito <113700624+HideinMito@users.noreply.github.com>
2022-12-02 21:07:20 +0100

    Add files via upload

---
+++ González_Siles_Roldán/hola_mundo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Mi primer hola mundo</title>
+
+</head>
+
+<body>
+
+    <?php
+            echo "Hola mundo";
+    ?>
+    
+</body>
+
+</html>
